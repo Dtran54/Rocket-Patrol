59ade40f32db4e6e374d00e809cc5fb90c576e31
dtran54 <dtran54@ucsc.edu>
2021-06-30 02:09:23 -0700

    added code comments

--- src/main.js
+++ src/main.js
@@ -13,3 +13,7 @@ let borderPadding = borderUISize / 3;
 
 // reserve keyboard vars
 let keyF, keyR, keyLEFT, keyRIGHT;
+
+// Rocket Patrol Mods, 6/30/2021, 12 hours in
+
+// 60 points for changing the theme and aesthetic, 20 points for a new ship type with a smaller hitbox, more speed, and more points.
